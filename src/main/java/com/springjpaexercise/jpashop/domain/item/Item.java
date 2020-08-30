1bb0798e782d337f48abdb6ca30306e4745c791f
SehyeongLee <shseoul14@gmail.com>
2020-08-30 16:38:06 +0900

    Add business logic on Item entity
    addStock
    removeStock(+exception)

--- src/main/java/com/springjpaexercise/jpashop/domain/item/Item.java
+++ src/main/java/com/springjpaexercise/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.springjpaexercise.jpashop.domain.item;
 
 import com.springjpaexercise.jpashop.domain.Category;
+import com.springjpaexercise.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,16 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //Business Logic
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
