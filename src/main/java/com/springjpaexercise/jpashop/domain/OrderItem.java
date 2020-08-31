0dd64ee7e05da3997a1a65b4df6efd218047eb5a
SehyeongLee <shseoul14@gmail.com>
2020-08-31 11:15:17 +0900

    Add createOrderItem function
    generate OrderItem at once
    prevent other way to make orderItem instance

--- src/main/java/com/springjpaexercise/jpashop/domain/OrderItem.java
+++ src/main/java/com/springjpaexercise/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package com.springjpaexercise.jpashop.domain;
 
 import com.springjpaexercise.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,6 +12,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id
@@ -28,4 +31,17 @@ public class OrderItem {
     private int orderPrice; // 주문가격
 
     private int count; // 주문량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
 }
