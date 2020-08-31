042bec793223b67886a12712ae0bf55f3c1a6ae1
SehyeongLee <shseoul14@gmail.com>
2020-08-31 18:15:14 +0900

    Add create book items function

---
+++ src/main/java/com/springjpaexercise/jpashop/controller/BookController.java
@@ -0,0 +1,40 @@
+package com.springjpaexercise.jpashop.controller;
+
+import com.springjpaexercise.jpashop.domain.item.Book;
+import com.springjpaexercise.jpashop.domain.item.Item;
+import com.springjpaexercise.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+
+}
