dce1636aa6fc527cf1915153c1abe5d6f19672ab
SehyeongLee <shseoul14@gmail.com>
2020-09-28 20:06:10 +0900

    Add MembersGet RestAPI

--- src/main/java/com/springjpaexercise/jpashop/api/MemberApiController.java
+++ src/main/java/com/springjpaexercise/jpashop/api/MemberApiController.java
@@ -10,12 +10,33 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    // 이경우에도 Entity를 직접 노출시키면 Entity의 칼럼명이 수정된다거나, 필요없는 모든 데이터가 같이 딸려오기때문에
+    // 여기도 DTO를 사용하는 것이 좋다.
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+
 
     // Entity를 바로 파라미터로 받으면 누군가가 Entity 컬럼명을 수정하면 API Spec 자체가 바뀌어 문제가 생긴다.
     // Entity는 사용 되는 곳이 많아서 수정 가능성이 높다.
@@ -72,6 +93,17 @@ public class MemberApiController {
 
     @Data
     static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
         private String name;
     }
 }
